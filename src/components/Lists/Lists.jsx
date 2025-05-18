43c7bea7564c59c4d5bd94588d7ff28d6959d0dc
Eddy Rhenals <eddy.rhenals7@gmail.com>
2025-05-17 20:29:54 -0500

    Create Item and Item Group

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import List from './List'
+import { Item } from 'semantic-ui-react'
 
 function Lists({lists}) {
   return (
     <div>
-      {lists.map(item => <li key={item.id}>{item.title}</li>)}
+        <Item.Group>
+        {lists.map(item => 
+            <List key={item.id} {...item}>
+                {item.title}
+            </List>)}
+        </Item.Group>
     </div>
   )
 }
